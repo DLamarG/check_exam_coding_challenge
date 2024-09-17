ae322d5dbcc4d96eeeceada2c3e6baecda869fde
DLamarG <demondg8@gmail.com>
2024-09-16 20:49:38 -0500

    added test

---
+++ checkExam.js
@@ -0,0 +1,10 @@
+function checkExam(arr1, arr2){
+
+}
+
+
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]))
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]))
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))
